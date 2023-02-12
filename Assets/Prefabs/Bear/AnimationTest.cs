c7d0d453b8a54870854bc2c2fc375cf9a6f7a375
Eazue <99573770+Eazue@users.noreply.github.com>
2023-02-11 20:00:56 -0800

    Step5_Bear_Animation

---
+++ Assets/Prefabs/Bear/AnimationTest.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationTest : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //// run
+
+        //if (Input.GetKeyUp(KeyCode.W))
+        //{
+        //    ResetAllBool();
+        //    animator.SetBool("Idle", true);
+        //}
+
+        // Jump
+        //if (characterController.isGrounded)
+        //{
+        //    if (Input.GetButtonDown("Jump"))
+        //    {
+        //        animator.SetBool("Jump", true);
+        //    }
+        //}
+    }
+
+
+
+}
--- Assets/Scripts/TestCharController.cs
+++ Assets/Scripts/TestCharController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class TestCharController : MonoBehaviour
@@ -8,24 +9,106 @@ public class TestCharController : MonoBehaviour
     public float movementSpeed = 10f;
     public SpawnManager spawnManager;
 
+    public float rotationSpeed;
+    public float jumpSpeed;
+    public CharacterController characterController;
+    private float ySpeed;
+    public Animator animator = new Animator();
+    //    public Rigidbody myRigidbody;
+    private bool isJump = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // characterController= GetComponent<CharacterController>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
+        // forward
         float vMovement = Input.GetAxis("Vertical") * movementSpeed;
 
-        transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
+        // run forward
+        if (!isJump)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)
+                || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            {
+                ResetAllBool();
+                animator.SetBool("Run Forward", true);
+
+            }
+        }
+        //if (hMovement > 0 || vMovement > 0)
+        //{
+        //    animationTest.animator.SetBool("Run Forward", true);
+        //} else
+        //{
+        //    animationTest.ResetAllBool();
+        //    animationTest.animator.SetBool("Idle", true);
+        //}
+
+        // transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
+        // https://www.youtube.com/watch?v=BJzYGsMcy8Q
+        Vector3 movementDirection = new Vector3(hMovement, 0, vMovement);
+        StopAnimation(movementDirection);
+        Vector3 velocity = movementDirection;
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            if (isJump) 
+            {
+                animator.SetBool("Run Forward", true);
+                isJump = false;
+            } else {
+                ySpeed = -0.5f;
+                if (Input.GetButtonDown("Jump"))
+                {
+                    // myRigidbody.velocity = Vector3.up * 7;
+                    ySpeed = jumpSpeed;
+                    ResetAllBool();
+                    animator.SetBool("Jump", true);
+                    isJump = true;
+                }
+            }
+            
+        }
+            // transform.Translate(velocity);
+            velocity.y = ySpeed;
+            characterController.Move(velocity * Time.deltaTime);
+        
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            // transform.forward = movementDirection;
+        }
     }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         spawnManager.SpawnTriggerEntered();
     }
-
+    
+    // Char Animation
+    public void StopAnimation(Vector3 movement)
+    {
+        if (movement == Vector3.zero && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)
+            && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            ResetAllBool();
+            animator.SetBool("Idle", true);
+        }
+    }
+    public void ResetAllBool()
+    {
+        animator.SetBool("Run Forward", false);
+        animator.SetBool("Idle", false);
+        animator.SetBool("Jump", false);
+    }
 }
